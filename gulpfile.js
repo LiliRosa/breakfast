fc8e22b8cfd8a159caf7460ab7f90595c66d0ffe
Lili <lilidrs13@gmail.com>
2020-03-24 22:18:36 +0000

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-var gulp          = require('gulp');
-var browserSync   = require('browser-sync').create();
-var $             = require('gulp-load-plugins')();
-var autoprefixer  = require('autoprefixer');
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var $ = require('gulp-load-plugins')();
+var autoprefixer = require('autoprefixer');
 
 var sassPaths = [
   'node_modules/foundation-sites/scss',
@@ -11,12 +11,14 @@ var sassPaths = [
 function sass() {
   return gulp.src('scss/app.scss')
     .pipe($.sass({
-      includePaths: sassPaths,
-      outputStyle: 'compressed' // if css compressed **file size**
-    })
+        includePaths: sassPaths,
+        outputStyle: 'expanded' // if css compressed **file size**
+      })
       .on('error', $.sass.logError))
     .pipe($.postcss([
-      autoprefixer({ browsers: ['last 2 versions', 'ie >= 9'] })
+      autoprefixer({
+        browsers: ['last 2 versions', 'ie >= 9']
+      })
     ]))
     .pipe(gulp.dest('css'))
     .pipe(browserSync.stream());
